6b12efb8f6d6d453480b1ebb65443c0da19234c3
henryqla <henry.q.lam05@gmail.com>
2026-05-21 13:25:43 -0700

    added libary and sensor to physical.py

--- onboard/physical/physical.py
+++ onboard/physical/physical.py
@@ -1,21 +1,22 @@
-import time
 import busio
 import board
 import time
 from gpiozero import Servo
+import ms5837
 
 class ROV:
     def __init__(self):
         self.servos = {}
         try:
             i2c = busio.I2C(board.SCL, board.SDA)
+            self.bar02 = ms5837.MS5837_02BA()
         except Exception as e:
             print(e)
             print("UNABLE TO CONNECT TO IMU")
 
     # number = GPIO number
     # value = PWM value
-    def set_pin_pwm(self, number: int, value: int): 
+    def set_pin_pwm(self, number: int, value: int):
         # print(number, value)
         if number not in self.servos:
             self.servos[number] = Servo(number)
@@ -27,8 +28,22 @@ class ROV:
     async def flush_pin_pwms(self):
         pass
 
+    # get depth function, heavily based on quinns imu function
+    def get_depth(self) -> dict:
+        if self.bar02 is None:
+            return {}
+        try:
+            depth = self.bar02.depth
+            if depth[0] is None:
+                return {}
+            return {"depth": depth}
+        except:
+            return {}
+
     async def poll_sensors(self) -> dict:
         readings = []
+
+        readings.append(self.get_depth)
 
         # Will be a list of (maybe empty) dictionaries of readings to report
         readings_dict = {}
