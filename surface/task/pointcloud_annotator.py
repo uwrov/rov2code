6e7174411b0245e76e2882e27dceb9919a11636d
ImantsS <imants.smidchens@gmail.com>
2026-06-12 11:44:58 -0700

    measure points with left click & shift left click

--- surface/task/pointcloud_annotator.py
+++ surface/task/pointcloud_annotator.py
@@ -38,13 +38,18 @@ def load_glb_pointcloud(path:str) -> PointCloudData:
 
     return PointCloudData(points=points, colors = colors)
 
+@dataclass
+class SelectionState:
+    p1_idx: int | None = None
+    p2_idx: int | None = None
+
 class PointCloudViewer:
     def __init__(self, data: PointCloudData):
         self.data = data
         self.cloud = pv.PolyData(data.points)
         self.cloud["rgb"] = data.colors
         self.plotter = pv.Plotter()
-        self.selected_idx = None
+        self.selection = SelectionState()
         self.setup_scene()
     def setup_scene(self):
         self.plotter.add_points(
@@ -76,29 +81,103 @@ class PointCloudViewer:
             show_message=False,
             tolerance=0.01,
         )
+    def is_shift_pressed(self) -> bool:
+        iren = self.plotter.iren
+        interactor = getattr(iren, "interactor", iren)
+        return bool(interactor.GetShiftKey())
     
     def on_pick(self, picked_point, picker):
         idx = picker.GetPointId()
         if idx < 0:
             return
-        self.selected_idx = idx
-        point = self.data.points[idx]
-        # color = self.data.colors[idx]
-        # print(f"color={color}")
-        self.show_selected_point(point)
+        if self.is_shift_pressed():
+            self.set_p2(idx)
+        else:
+            self.set_p1(idx)
+        self.update_active_annotation()
 
-    def show_selected_point(self, point):
-        self.plotter.remove_actor("selected_point", render=False)
-        marker = pv.PolyData(np.array([point]))
+    def set_p1(self, idx):
+        self.selection.p1_idx = idx
+        self.selection.P2_idx = None
+        p = self.data.points[idx]
+        # print(p)
+
+    def set_p2(self, idx):
+        if self.selection.p1_idx == None:
+            return
+        self.selection.p2_idx = idx
+        p = self.data.points[idx]
+        # print(p)
+        self.print_measurement()
+    
+    def print_measurement(self):
+        p1 = self.data.points[self.selection.p1_idx]
+        p2 = self.data.points[self.selection.p2_idx]
+        delta = p2 - p1
+        dist = np.linalg.norm(delta)
+        print(f"dx={delta[0]}")
+        print(f"dy={delta[1]}")
+        print(f"dz={delta[2]}")
+        print(f"d ={dist}")
+
+    def update_active_annotation(self):
+        self.plotter.remove_actor("p1_marker", render=False)
+        self.plotter.remove_actor("p2_marker", render=False)
+        self.plotter.remove_actor("active_line", render=False)
+        self.plotter.remove_actor("active_label", render=False)
+
+        if self.selection.p1_idx is None:
+            self.plotter.render()
+            return
+        p1 = self.data.points[self.selection.p1_idx]
         self.plotter.add_points(
-            marker,
+            pv.PolyData(np.array([p1])),
             color="red",
             point_size=12,
             render_points_as_spheres=True,
-            name="selected_point",
+            name="p1_marker",
+            pickable=False,
+        )
+        if self.selection.p2_idx is None:
+            self.plotter.render()
+            return
+        p2 = self.data.points[self.selection.p2_idx]
+        self.plotter.add_points(
+            pv.PolyData(np.array([p2])),
+            color="blue",
+            point_size=12,
+            render_points_as_spheres=True,
+            name="p2_marker",
+            pickable=False,
+        )
+        self.plotter.add_lines(
+            np.array([p1, p2]),
+            color="red",
+            width=4,
+            name="active_line",
+            connected=True,
+        )
+        delta = p2 - p1
+        dist = np.linalg.norm(delta)
+        mid = (p1+p2)/2
+        label = (
+            f"d ={dist}\n"
+            f"dx={delta[0]}\n"
+            f"dy={delta[1]}\n"
+            f"dz={delta[2]}\n"
+        )
+        self.plotter.add_point_labels(
+            np.array([mid]),
+            [label],
+            name = "active_label",
+            font_size = 14,
+            point_size=0,
+            shape_opacity=0.5,
+            always_visible=True,
             pickable=False,
         )
         self.plotter.render()
+        
     def run(self):
         self.plotter.show()
     
